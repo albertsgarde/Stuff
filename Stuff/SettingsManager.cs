9548206d33497bd63f8a7e270f16063dc19d2c6c
PenguinAgen <albertsgarde@gmail.com>
2018-07-20 15:19:04 +0200

    Add InvalidSettingFormat

--- Stuff/SettingsManager.cs
+++ Stuff/SettingsManager.cs
@@ -84,7 +84,7 @@ namespace Stuff
             return settings[category][key][index];
         }
 
-        public string[] GetStrings(string category, string key)
+        public IEnumerable<string> GetStrings(string category, string key)
         {
             if (!settings.ContainsKey(category))
                 throw new CategoryNotFoundException(category, key);
@@ -95,57 +95,72 @@ namespace Stuff
 
         public int GetInt(string category, string key, int index = 0)
         {
-            return int.Parse(GetString(category, key, index));
+            if (int.TryParse(GetString(category, key, index), out int result))
+                return result;
+            else
+                throw new InvalidSettingFormat(category, key, index);
         }
 
         public IEnumerable<int> GetInts(string category, string key)
         {
-            foreach (string value in settings[category][key])
-                yield return int.Parse(value);
+            for (int i = 0; i < settings[category][key].Length; ++i)
+                yield return GetInt(category, key, i);
         }
 
         public long GetLong(string category, string key, int index = 0)
         {
-            return long.Parse(GetString(category, key, index));
+            if (long.TryParse(GetString(category, key, index), out long result))
+                return result;
+            else
+                throw new InvalidSettingFormat(category, key, index);
         }
 
         public IEnumerable<long> GetLongs(string category, string key)
         {
-            foreach (string value in settings[category][key])
-                yield return long.Parse(value);
+            for (int i = 0; i < settings[category][key].Length; ++i)
+                yield return GetLong(category, key, i);
         }
 
         public float GetFloat(string category, string key, int index = 0)
         {
-            return float.Parse(GetString(category, key, index));
+            if (float.TryParse(GetString(category, key, index), out float result))
+                return result;
+            else
+                throw new InvalidSettingFormat(category, key, index);
         }
 
         public IEnumerable<float> GetFloats(string category, string key)
         {
-            foreach (string value in settings[category][key])
-                yield return float.Parse(value);
+            for (int i = 0; i < settings[category][key].Length; ++i)
+                yield return GetFloat(category, key, i);
         }
 
         public double GetDouble(string category, string key, int index = 0)
         {
-            return double.Parse(GetString(category, key, index));
+            if (double.TryParse(GetString(category, key, index), out double result))
+                return result;
+            else
+                throw new InvalidSettingFormat(category, key, index);
         }
 
         public IEnumerable<double> GetDoubles(string category, string key)
         {
-            foreach (string value in settings[category][key])
-                yield return double.Parse(value);
+            for (int i = 0; i < settings[category][key].Length; ++i)
+                yield return GetDouble(category, key, i);
         }
 
-        public bool GetBoolean(string category, string key, int index = 0)
+        public bool GetBool(string category, string key, int index = 0)
         {
-            return bool.Parse(GetString(category, key, index));
+            if (bool.TryParse(GetString(category, key, index), out bool result))
+                return result;
+            else
+                throw new InvalidSettingFormat(category, key, index);
         }
 
-        public IEnumerable<bool> GetBooleans(string category, string key)
+        public IEnumerable<bool> GetBools(string category, string key)
         {
-            foreach (string value in settings[category][key])
-                yield return bool.Parse(value);
+            for (int i = 0; i < settings[category][key].Length; ++i)
+                yield return GetBool(category, key, i);
         }
     }
 }
