3143112d89004d6e292eda08c8d17fe9810a0c54
PenguinAgen <albertsgarde@gmail.com>
2018-07-23 13:45:33 +0200

    Add two classes LoaderType and TypeLoader to aid in loading classes at runtime

---
+++ Stuff/LoaderTypes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stuff
+{
+    public class LoaderTypes<T> : IEnumerable<XMLTypeLoader<T>>
+    {
+        private readonly Dictionary<string, XMLTypeLoader<T>> moduleTypes;
+
+        public LoaderTypes(PathList paths, string elementName)
+        {
+            foreach (var file in paths)
+            {
+                foreach (var element in XMLUtil.FindElements(file, e => e.Name == elementName))
+                {
+                    var mt = new XMLTypeLoader<T>(element);
+                    moduleTypes[mt.Name] = mt;
+                }
+            }
+        }
+
+        public XMLTypeLoader<T> this[string typeName] => moduleTypes[typeName];
+
+        public IEnumerator<XMLTypeLoader<T>> GetEnumerator()
+        {
+            return moduleTypes.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Stuff/TypeLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Stuff
+{
+    /// <summary>
+    /// Loads a class from an xmlfile that references the class and creates an instance.
+    /// </summary>
+    /// <remarks>The loaded types must have a default constructor</remarks>
+    public class XMLTypeLoader<T>
+    {
+        public string ClassPath { get; }
+
+        public string Name { get; }
+
+        public T Instance { get; }
+
+        /// <summary>
+        /// Loads the type from an XElement.
+        /// The element should have two children:
+        ///     A "name" element, designating the internally used name of the type. Should be unique, but does not have to be the same as the class name.
+        ///     A "classPath" element, designating the location where the class is found.
+        /// </summary>
+        /// <param name="element">An XElement that holds information about the location of the class to be loaded and what name to give the type.</param>
+        public XMLTypeLoader(XElement element)
+        {
+            ClassPath = element.ElementValue("classPath");
+            Name = element.ElementValue("name");
+            Instance = (T)Activator.CreateInstance(Type.GetType(ClassPath));
+        }
+    }
+}
