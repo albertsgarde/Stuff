d68b3ce9950663156f176aa10b148574967c64d7
albert <albertsgarde@gmail.com>
2018-09-24 13:16:01 +0200

    Moved Music back into Stuff

---
+++ Stuff/Music/Key.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthLib.Music
+{
+    public class Key
+    {
+        public Scale Scale { get; }
+        
+        public Tone Root { get; }
+
+        public Key (Tone note, Scale scale)
+        {
+            Root = note;
+            Scale = scale;
+        }
+
+        public Tone Step(int step, int modifier)
+        {
+            return Root + Scale.Step(step) + modifier;
+        }
+    }
+}
