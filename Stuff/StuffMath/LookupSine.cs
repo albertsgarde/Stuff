5b5c579506469e9675a88f3767cecc60fb1988ef
albert <albertsgarde@gmail.com>
2018-09-25 11:26:25 +0200

    Add LookupSine class

---
+++ Stuff/StuffMath/LookupSine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stuff.StuffMath
+{
+    public class LookupCosine
+    {
+        public float[] Table { get; }
+
+        public LookupCosine(int length)
+        {
+            Table = new float[length];
+            var increment = Math.PI * 2 / length;
+            for (int i = 0; i < length; ++i)
+                Table[i] = (float)Math.Cos(increment * i);
+        }
+    }
+}
