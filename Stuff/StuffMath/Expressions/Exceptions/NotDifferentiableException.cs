d736d6027119481110e931e17ebfea38f5e1609f
albert <albertsgarde@gmail.com>
2018-12-25 11:39:47 +0100

    Add NotDifferentiableException

---
+++ Stuff/StuffMath/Expressions/Exceptions/NotDifferentiableException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stuff.StuffMath.Expressions
+{
+    [Serializable()]
+    public class NotDifferentiableException : Exception
+    {
+        public NotDifferentiableException() : base()
+        {
+        }
+
+        public NotDifferentiableException(string message) : base(message)
+        {
+        }
+
+        protected NotDifferentiableException(SerializationInfo si, StreamingContext sc) : base(si, sc) { }
+    }
+}
